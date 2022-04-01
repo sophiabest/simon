78ae006bc979e5d70855118ce83684bd3296f0c0
sophiabest <bestsophia1014@gmail.com>
2022-04-01 19:20:12 -0400

    state variables

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let compPattern;
+let playerChoices;
+let interval;
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+document.querySelector('main').addEventListener('click', handleButton);
+/*----- functions -----*/
+init();
+
+function init() {
+    compPattern = {
+
+    };
+}
+
