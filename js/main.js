b29340b2c1f9f981fd02faceb48f04f265610f49
sophiabest <bestsophia1014@gmail.com>
2022-04-03 18:55:07 -0400

    startgame defined

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /*----- constants -----*/
-
+delay;
 
 /*----- app's state (variables) -----*/
 let compPattern;
 let playerChoices;
 let interval;
+let lose;
 /*----- cached element references -----*/
-
 
 /*----- event listeners -----*/
 document.querySelector('main').addEventListener('click', handleButton);
@@ -16,7 +16,18 @@ init();
 function init() {
     compPattern = {};
     playerChoices = {};
+    interval = 0;
+    lose = 'true';
     
-
 }
 
+function startGame() {
+    if (playerRound < 1) {
+        flashLights(500);
+    }
+    playerChoice = [];
+    playerRound += 1;
+    getRandom();
+    setTimeout(() => compPattern(), 500);
+    render();
+}
