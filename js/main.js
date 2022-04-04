44c353ea431fea8afee3a772b92c173ed72091d4
sophiabest <bestsophia1014@gmail.com>
2022-04-04 14:45:03 -0400

    change the js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 /*----- constants -----*/
-//delay;
+const colors = [0, 1, 2, 3];
 
 /*----- app's state (variables) -----*/
 let compPattern;
 let playerChoices;
 let interval;
 let lose;
-let compTurn;
-let playerTurn;
-let playerRound;
-let getRandom;
+let round;
 //let matchPattern;
 
 /*----- cached element references -----*/
-const msgEl = document.querySelector('h2');
-const startBtn = document.getElementById('startBtn');
 
 /*----- event listeners -----*/
 //document.querySelector('main').addEventListener('click', handleButton);
@@ -22,53 +17,30 @@ const startBtn = document.getElementById('startBtn');
 /*----- functions -----*/
 init();
 
-function init() {
-    if (playerRound < 1) {
+function init () {
+    let round = 0;
+    let compPattern = {};
+    let playerChoices = {};
+    let lose = true; 
+}
+
+function render() {
+    createPattern(compPattern);
+    blink(compPattern);
+}
+
+for (var i = 0; i < playerChoices.length; i++) {
+    if (JSON.stringify(compPattern) == JSON.stringify(playerChoices)) {
+        playerChoices = [];
+        round++;
+        roundCounter.innterHTML = round;
+        render(); 
+    } else if (playerChoices[i] !== compPattern[i]) {
+        alert(`Game Over!`);
     }
-    playerChoice = [];
-    playerRound += 1;
-    //getRandom();
-    //render();
-}
+ }
 
+ function createPattern = (array) => {
+     array.push(colors[Math.floor(Math.random() * colors.length)]);
+ };
 
-function matchPattern() {
-    if(compPattern.length === playerPattern.length) {
-        for(let i=0;i<compPattern.length;i++) 
-        if(compPattern[i]!=playerPattern[i]) {
-            playerPattern = [];
-            const gameElement = document.querySelector('.game');
-        } else {
-            
-        }
-        if (playerRound === totalRounds) {
-            lose = false;
-            
-            playerRound = 0;
-            compPattern = [];
-            interval = 500;
-        }
-    } 
-} 
-
-
-//function getRandom() {
-    //compPattern.push(Math.floor(Math.random()*4)+1);
-//}
-
-function CompPattern() {
-    delayFlash(compPattern);
-}
-
-async function delayFlash(array){
-    for(let i=0; i<array.length; i++) {
-        
-    }
-}
-
-//function flashColor(i) {
-   // if (i === 1) redFlash();
-  //  if (i === 2) blueFlash();
-  //  if (i === 3) yellowFlash();
-  //  if (i === 4) greenFlash();
-//}
